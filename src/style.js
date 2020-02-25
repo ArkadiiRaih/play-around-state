a5bed6777719be75050912764b5a993800c148b3
ArkadiiRaih <ArkadioRaih@yandex.ru>
2020-02-25 21:54:15 +1000

    Added darken and lighten helper

--- src/style.js
+++ src/style.js
@@ -7,8 +7,8 @@ export const colors = {
   fontColor: "#28283d"
 };
 
-export const darken = (color, amt) => color;
-export const lighten = (color, amt) => color;
+export const darken = (color, amt) => LightenDarkenColor(color, -amt);
+export const lighten = (color, amt) => LightenDarkenColor(color, -amt);
 
 export const inputStyle = color => css`
   background-color: ${color};
@@ -68,3 +68,31 @@ export const global = css`
     ${inputStyle(colors.white)};
   }
 `;
+
+function LightenDarkenColor(col, amt) {
+  var usePound = false;
+
+  if (col[0] === "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(col, 16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00ff) + amt;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  var g = (num & 0x0000ff) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
