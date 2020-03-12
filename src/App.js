294df4b7f75739cb827ab8e7f4dee291ebf3d5c0
ArkadiiRaih <ArkadioRaih@yandex.ru>
2020-03-13 04:28:50 +1000

    Store state in local storage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
 import React from "react";
 import "./App.css";
 import { Main, Controls, Count, Control } from "./components";
-import { connect } from "react-redux";
-import { increment, decrement, reset } from "./redux";
 
-function App({ count, increment, decrement, reset }) {
-  return (
-    <Main className="counter">
-      <Count className="count">{count}</Count>
-      <Controls className="controls">
-        <Control onClick={increment}>Increment</Control>
-        <Control onClick={decrement}>Decrement</Control>
-        <Control onClick={reset}>Reset</Control>
-      </Controls>
-    </Main>
-  );
-}
-
-const mapStateToProps = state => state;
-const mapDispatchToProps = {
-  increment,
-  decrement,
-  reset
+const getStateFromLocalStorage = state => {
+  const storage = localStorage.getItem("counterStorage");
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+const setStateToLocalStorage = state => {
+  localStorage.setItem("counterStorage", JSON.stringify(state));
+  return state;
+};
+
+const setStatetoTitle = state => {
+  document.title = `Counter: ${state.count}`;
+  return state;
+};
+
+function compose(...fns) {
+  return function(arg) {
+    fns.reduceRight((y, f) => f(y), arg);
+  };
+}
+
+const callback = compose(setStateToLocalStorage, setStatetoTitle);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocalStorage();
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState(
+      state => ({ count: state.count + 1 }),
+      () => callback(this.state)
+    );
+  }
+
+  decrement() {
+    this.setState(
+      state => ({ count: state.count - 1 }),
+      () => callback(this.state)
+    );
+  }
+
+  reset() {
+    this.setState({ count: 0 }, () => callback(this.state));
+  }
+
+  render() {
+    return (
+      <Main className="counter">
+        <Count className="count">{this.state.count}</Count>
+        <Controls className="controls">
+          <Control onClick={this.increment}>Increment</Control>
+          <Control onClick={this.decrement}>Decrement</Control>
+          <Control onClick={this.reset}>Reset</Control>
+        </Controls>
+      </Main>
+    );
+  }
+}
+
+export default App;
